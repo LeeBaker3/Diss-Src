092257b9dd7c511e23bf0a8a4bd4325c7844d130
Lee Baker <lee.baker1@ymail.com>
2014-09-09 00:17:55 +1000

    Working updates to upload

--- AtomicInfromationConfigManager2/src/main/java/filemanagement/UploadFile.java
+++ AtomicInfromationConfigManager2/src/main/java/filemanagement/UploadFile.java
@@ -7,10 +7,12 @@
 package filemanagement;
 
 import com.mycompany.atomicinformationconfigurationmanager.entities.Artefact.ArtefactController;
+import com.mycompany.atomicinformationconfigurationmanager.entities.util.JsfUtil;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
+import java.util.ResourceBundle;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -40,37 +42,55 @@ public class UploadFile {
     }
     
     public String upload() throws IOException{   
-        InputStream inputStream = file.getInputStream();
-        FileOutputStream outputStream = new FileOutputStream(getFilename(file));
-        
-        byte[] buffer = new byte[4096];
-        int bytesRead = 0;
-        while(true){
-            bytesRead = inputStream.read(buffer);
-            if(bytesRead > 0){
-                outputStream.write(buffer, 0, bytesRead);
+    try {
+            InputStream inputStream = file.getInputStream();
+            FileOutputStream outputStream = new FileOutputStream(getFilename(file));
+            Long fileSizeLong;
+            fileSizeLong = file.getSize();
+
+            if(fileSizeLong > (long)Integer.MAX_VALUE){
+                return "FileToBig";
             }
-            else {
-                break;
-            }  
+            else
+            {
+                int fileSizeInt;
+                fileSizeInt = fileSizeLong.intValue();
+
+                byte[] buffer = new byte[fileSizeInt];
+                int bytesRead = 0;
+
+                while(true){
+                    bytesRead = inputStream.read(buffer);
+                    if(bytesRead > 0){
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                    else {
+                        break;
+                    }  
+                }
+                outputStream.close();
+                inputStream.close();
+
+                artefactController.getCurrent().setArtefactFilename(getFilename(file));
+                artefactController.getCurrent().setArtefactFile(buffer);
+                artefactController.update();
+                uploadODF();
+                JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ArtefactUpload"));
+                return "Success";
+            }
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("UploadFailed"));
+            return "Success";
         }
-        outputStream.close();
-        inputStream.close();
-        
-        artefactController.getCurrent().setArtefactFilename(getFilename(file));
-        artefactController.getCurrent().setArtefactFile(buffer);
-        artefactController.update();
-        
-        return "Success";
     }
 
     public String uploadODF() throws IOException{
         try {
             TextDocument oldDocument = TextDocument.newTextDocument();
-//            InputStream inputStream = file.getInputStream();
-//            oldDocument = TextDocument.loadDocument(inputStream);
-//            oldDocument.addParagraph("New text added");
-//            oldDocument.save("C:\\Users\\Lee Baker\\Desktop\\TestFileUpdate.odt");
+            InputStream inputStream = file.getInputStream();
+            oldDocument = TextDocument.loadDocument(inputStream);
+            oldDocument.addParagraph("New text added");
+            oldDocument.save("C:\\Users\\Lee Baker\\Desktop\\TestFileUpdate.odt");
             // TO Be completed out
         } catch (Exception e) {
             
