f641b7dd94c630552c93be4491a3f55ce1597e88
LeeBaker3 <lee@Lee-Bakers-MacBook-Pro.local>
2014-09-19 00:09:31 +1000

    Updates for storing Scan Atomic Information

--- AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/artefactdistribution/Artefactdistribution_.java
+++ AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/artefactdistribution/Artefactdistribution_.java
@@ -9,7 +9,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-16T23:56:10")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-19T00:02:52")
 @StaticMetamodel(Artefactdistribution.class)
 public class Artefactdistribution_ extends BaseEntity_ {
 
--- AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/distributionrecipient/Distributionrecipient_.java
+++ AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/distributionrecipient/Distributionrecipient_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-16T23:56:10")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-19T00:02:52")
 @StaticMetamodel(Distributionrecipient.class)
 public class Distributionrecipient_ extends BaseEntity_ {
 
