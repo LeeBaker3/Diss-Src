39dbe449927e7133ac9d635044a4a9d1783f3c4c
LeeBaker3 <lee@192.168.0.9>
2014-09-23 23:29:50 +1000

    Modified/added comments 22/09/14

--- AtomicInfromationConfigManager2/src/main/java/scan/CreateArtefactAtomicInfoRecords.java
+++ AtomicInfromationConfigManager2/src/main/java/scan/CreateArtefactAtomicInfoRecords.java
@@ -23,18 +23,25 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
+ * CreateArtefactAtomicInfoRecords Class. The purpose of this class is to identify an of a List<> Atomicinfromation entities 
+ * passed to the class has previously been identified for an Artefact entity. For the Atomicinfromation entities that haven't 
+ * previously been identified they are reported to a 3rd party. The class also implements a method to create new 
+ * Artefactatomicinformation entities and persist them to the database for the newly identified Atomicinfromation entities.
+ * 
  * @author Lee Baker
- *
+ * @version 1.0
  */
+
+//START LEE BAKER GENERATED CODE
 @Stateless
 @Named("createArtefactAtomicInfoRecords")
 public class CreateArtefactAtomicInfoRecords {
 
     
-    private List<Atomicinformation> foundItems;
-    private Map<Atomicinformation, Boolean> checked = new HashMap<>();
-    private List<Atomicinformation> newItems;
-    private int size;
+    private List<Atomicinformation> foundItems;//A List<> of Atomicinformation entities that will be checked to identify if they have previously been identified.
+    private Map<Atomicinformation, Boolean> checked = new HashMap<>();//Used to get the Atomicinformation entities that have been selected tp create Artefactatomicinformation.
+    private List<Atomicinformation> newItems;// A List<> of Atomicinformation entities that will be used to create Artefactatomicinformation entities.
+    private int size;// Size is used to check that there are some newly identified Atomicinformation entities.
     @Inject
     ArtefactatomicinformationController artefactatomicinformationController;
     @Inject
@@ -78,7 +85,14 @@ public class CreateArtefactAtomicInfoRecords {
         this.newItems = newItems;
     }
     
-    public String createInfoRecords (List<Atomicinformation> foundItems, Artefact artefact){ 
+    /**
+     * createInfoRecords method. This method is used to control the identify of new Atomicinformation from the ones previously found
+     * and set the size property to the number of newly identified Atomicinformation entities.
+     * 
+     * @param foundItems List<> of Atomicinformation entities to be checked
+     * @return a string representing the JSF page to be displayed
+     */
+    public String createInfoRecords (List<Atomicinformation> foundItems){ 
         setFoundItems(foundItems);
         newItemsFound();
         setSize(newItems.size());
@@ -86,11 +100,19 @@ public class CreateArtefactAtomicInfoRecords {
         return "/Faces/artefactatomicinformation/ScanList";
     }
     
+    /**
+     * newItemsFound method. The purpose of this method is extract from the List<> of Atomicinfromation entities FoundItems
+     * Atomicinfromation entities that haven't previously been identified for a given artefact and a return a List<> of 
+     * Atomicinfromation that newly are discovered
+     * 
+     * @return List<> of Atomicinfromation that are newly discovered
+     */
     private String newItemsFound(){
         List<Atomicinformation> items = getFoundItems();
         newItems = new ArrayList<>();
         List<Atomicinformation> currentItemsAtomic = new ArrayList<>();
         
+        //Get List of existing Artefactatomicinformation entities for given Artefact entity and extract Atomicinformation entities
         try{
             List<Artefactatomicinformation> currentItems = artefactatomicinformationController.getSaveRetrieve().findByEntityActiveAndArtefactIDAndIsCurrentVersion(true, artefactController.getCurrent(), true);
             if (currentItems.isEmpty() == false){
@@ -99,6 +121,9 @@ public class CreateArtefactAtomicInfoRecords {
                 }
             }
             
+            /*  If their are Artefactatomicinformation entities for given Artefact entity then check if any the FoundItems Atomicinformation is
+            /   contained in the list. If no match is found then add the atomic information to a List<> of newly found Atomicinformation entities
+            */
             if (currentItems.isEmpty() == false){
                 for (Atomicinformation atomic: items){
                     if (currentItemsAtomic.contains(atomic) == false){
@@ -119,7 +144,14 @@ public class CreateArtefactAtomicInfoRecords {
         }
         return returnToJSFPage.returnToArtefactView();
     }
-
+    
+    /**
+     * create method. The purpose of this method is to take the selected Atomicinfromation entities from 3rd party, usually a JSF page 
+     * and create new Artefactatomicinformation entities that establishes the relationship between the the Artefact entity and the 
+     * Atomicinformation entity.
+     * 
+     * @return a string for the path of the JSF page to be navigated too
+     */
     public String create() {
         try{
             for(Atomicinformation item: newItems){
@@ -135,6 +167,12 @@ public class CreateArtefactAtomicInfoRecords {
         catch (Exception e){
             JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("ScanListArtefactatomicinformationFail"));
         }
+        finally{
+            checked.clear();
+            newItems.clear();
+        }
+        
         return returnToJSFPage.returnToArtefactView();
     }
 }
+//END LEE BAKER GENERATED CODE
--- AtomicInfromationConfigManager2/src/main/java/scan/Scan.java
+++ AtomicInfromationConfigManager2/src/main/java/scan/Scan.java
@@ -21,6 +21,20 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.ini4j.InvalidFileFormatException;
 
+/**
+ * Scan Class. The purpose of this abstract class is to scan a file in an attempt to find text that matches the text
+ * of the content attribute of an Artefact entity. The text is extracted from the file and 'cleaned' using a 'cleanString' 
+ * method and returned in a manner to allow easier matching against Atomicinformation entity content attribute. If any 
+ * Atomicinformation is found this is passed to a createArtefactAtomicInfoRecords class for processing other a null is returned.
+ * 
+ * The class implements an abstract method 'scan' which is implemented in different if derived classes for different file 
+ * formats. The class supports methods for checking that a file is available and of a support file format. 
+ * 
+ * @author Lee Baker
+ * @version 1.0
+ */
+
+//START LEE BAKER GENERATED CODE
 abstract public class Scan {
 
     @Inject
@@ -32,12 +46,18 @@ abstract public class Scan {
     @Inject 
     private CreateArtefactAtomicInfoRecords createArtefactAtomicInfoRecords;
 
-    //public List<Atomicinformation> scanArtefact(){
+    /**
+     * scanArtefact method. This is the main control method the class and coordinates the interaction of other methods to achieve the purpose
+     * of this class. 
+     * 
+     * @return String value that either be the navigation path to a JSF page to create database records of Atomicinformation entities found
+     * or null if an errors occurs or now Atomicinformation entities are found.
+     */
     public String scanArtefact(){    
-        List<Atomicinformation> listAtomicInfoAll;
-        List<Atomicinformation> listAtomicInfoFound;
-        Artefact artefact;
-        Project project;
+        List<Atomicinformation> listAtomicInfoAll;//List of all Atomicinformation entities related to the Project property of the class
+        List<Atomicinformation> listAtomicInfoFound;//List of Atomicinformation entities found in the artefactFile attribute of the 'artefact' property 
+        Artefact artefact; //Artefact entity that hold the file as an attribute artefactFile to be scanned
+        Project project;//Project entity that the Artefact and List<> of Atomicinformation belong too
 
         try {
             artefact = artefactController.getCurrent();
@@ -54,8 +74,7 @@ abstract public class Scan {
             }
             else
             {
-                return createArtefactAtomicInfoRecords.createInfoRecords(listAtomicInfoFound, artefact); 
-                //return listAtomicInfoFound;
+                return createArtefactAtomicInfoRecords.createInfoRecords(listAtomicInfoFound); 
             }
         } 
         catch (FileNotFoundException  e) {
@@ -72,8 +91,26 @@ abstract public class Scan {
         }
     } 
     
+    /**
+     * Scan method. This class is used to scan the file and return a list of Atomicinformation entities that
+     * have been found. This method is implemented differently in derived classed based on the file format.
+     * 
+     * @param artefact Artefact entity that hold the file as an attribute artefactFile to be scanned
+     * @param projectAtomicinfo List of all Atomicinformation entities to be searched for in file
+     * @return List of all Atomicinformation entities found in file
+     */
     abstract public List<Atomicinformation> scan (Artefact artefact, List<Atomicinformation> projectAtomicinfo);
     
+    /**
+     * cleanString method. This method is passed a string of text, the 'dirty string' and returns a 
+     * 'cleaned string'. The cleaning process involves removing white spaces, accents and making all
+     * characters lowercase. This process could be extend to remove punctuation as well.  
+     * 
+     * The purpose of this process is simplify matching between two strings.
+     * 
+     * @param dirtySrString a string that is unprocessed and is to be cleaned
+     * @return returns a string that has been cleaned
+     */
     protected String cleanString(String dirtySrString){
        String cleanString;
        cleanString = StringUtils.lowerCase(dirtySrString);
@@ -82,15 +119,29 @@ abstract public class Scan {
        return cleanString;
     }
     
+    /**
+     * fileAvailable method. Simple check to ensure that the Artefact entity has a Byte[] representation of the 
+     * file available to scan. If not an error is thrown FileNotFoundException
+     * 
+     * @param artefact Artefact entity to be checked
+     * @throws FileNotFoundException 
+     */
     protected void fileAvailable(Artefact artefact) throws FileNotFoundException {
             if (artefact.getArtefactFile() == null || artefact.getArtefactFile().length == 0){
                 throw new FileNotFoundException();
             }
     }
     
+    /**
+     * supportedFileType method. Simple check to ensure that the Artefact entity file format is of a type
+     * that is supported for scanning f not an error is thrown InvalidFileFormatException
+     * 
+     * @param artefact Artefact entity to be checked file attribute to be checked
+     * @throws InvalidFileFormatException 
+     */
     protected void supportedFileType(Artefact artefact) throws InvalidFileFormatException {
         String fileExt = FilenameUtils.getExtension(artefact.getArtefactFilename());
-        /*  This code could be extended to check for additional file types that are pulled from 
+        /*  This code could be extended/modified to check for additional file types that are pulled from 
         *   db table of config file
         */
         if (!"odt".equals(fileExt)){
@@ -98,3 +149,4 @@ abstract public class Scan {
         }
     }
 }
+//END LEE BAKER GENERATED CODE
