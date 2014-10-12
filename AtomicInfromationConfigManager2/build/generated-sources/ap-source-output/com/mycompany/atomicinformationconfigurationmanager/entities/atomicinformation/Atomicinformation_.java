a4f39f8a4df5d8ba93678260724601c4ece4d2f1
LeeBaker3 <lee@Lee-Bakers-MacBook-Pro.local>
2014-10-12 17:09:50 +1000

    Working Changes

--- AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/atomicinformation/Atomicinformation_.java
+++ AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/atomicinformation/Atomicinformation_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-22T23:05:12")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-12T17:08:10")
 @StaticMetamodel(Atomicinformation.class)
 public class Atomicinformation_ extends BaseEntity_ {
 
--- AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/typeofatomicinformation/Typeofatomicinformation_.java
+++ AtomicInfromationConfigManager2/build/generated-sources/ap-source-output/com/mycompany/atomicinformationconfigurationmanager/entities/typeofatomicinformation/Typeofatomicinformation_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-09-22T23:05:12")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-10-12T17:08:10")
 @StaticMetamodel(Typeofatomicinformation.class)
 public class Typeofatomicinformation_ extends BaseEntity_ {
 
